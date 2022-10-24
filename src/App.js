93124c7ae4849ce1bbaad37bcc4b17ee9ab5c14c
yusuparipin30 <yusuparipin30@gmail.com>
2022-10-24 16:17:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+ 
 function App() {
+  //dinamic value
+  const title = "welcome to my app";
+  const age = 20;
+  const link = "https://github.com/yusuparipin30/";
+
+  /*membuat ero function event untuk memanggil function ini bisa dari button  dengan cara 
+  nenambahkan onclick dan menggunakan dinamic value clickme*/
+  //cara nengkap parameter yusuf yang ada di button yaitu hanya perlu menambahkan name di function 
+  //dan memberikan pesan pada console.log yaitu hello name
+  const Clickme = (name) => {
+    console.log('Hello:'+ name);
+  }
+   //"penting" membuat EVENT di buttont yaitu onclick dan memanggil methot dengan ero function 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+     <h1>{title}</h1>
+     <h1>{age * 3}</h1>
+     <a href={link}>Go To Github Yusuf</a>
+     <button onClick={() => Clickme('Yusuf')}>Click me</button>
     </div>
   );
 }
-
+ 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*Peringatan: ReactDOM.render tidak lagi didukung di React 18. Gunakan createRoot sebagai gantinya.
+ Sampai Anda beralih ke API baru,aplikasi Anda akan berperilaku seolah-olah sedang menjalankan React 17.*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ /*Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API,
+  your app will behave as if it's running React 17. Learn more:*/
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ 
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+/*Untuk mengatasinya, Anda dapat kembali ke versi React sebelumnya atau memperbarui file index.js 
+agar selaras dengan sintaks React 18.*/
+
+/*To resolve it, you can either revert to a previous version of React or update your index.js file to align 
+with the React 18 syntax.*/
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
